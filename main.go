65e05a1419e421ffd63141c5dcb892e2332abf3a
Mark <mrivera1991@gmail.com>
2026-09-15 22:09:25 -0400

    Added comments for ideas of what I want this service to do

--- main.go
+++ main.go
@@ -24,5 +24,16 @@ func main() {
 	}
 
 	// Set up HTTP Handlers
+	// This server is evolving to handle initial upload requests and user registration
+    // Maybe in the future those domains can be split into their own services but for now
+    // its fine.
+
+    // Users
+	// Guests should be able to view register and login pages
+	// Users need to be able to logout, view profile, update profile
+
+    // Uploads
+	// Users should be able to provision an upload, check their upload's status, delete an upload, update metadata
+	// Users should be able to view videos uploaded by other users and themselves
 }
 
